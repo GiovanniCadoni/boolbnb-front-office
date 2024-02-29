41469b61ccc14f934b637ebfff45309659968c47
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-02-29 12:17:55 +0100

    add: carousel

--- src/pages/ApartmentDetails.vue
+++ src/pages/ApartmentDetails.vue
@@ -1,6 +1,13 @@
 <script>
 import axios from 'axios';
 import Map from '../components/Map.vue';
+import { Navigation, Pagination, Mousewheel, A11y, Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/mousewheel';
+import 'swiper/css/autoplay';
 
 export default {
     data() {
@@ -13,7 +20,21 @@ export default {
         };
     },
     name: 'App',
-    components: { Map },
+    components: { Map, Swiper, SwiperSlide },
+
+    setup() {
+        const onSwiper = (swiper) => {
+        //   console.log(swiper);
+        };
+        const onSlideChange = () => {
+        //   console.log('slide change');
+        };
+        return {
+          onSwiper,
+          onSlideChange,
+          modules: [Navigation, Pagination, Mousewheel, A11y, Autoplay],
+        };
+    },
 
     created() {
         this.loading = true;
@@ -34,7 +55,34 @@ export default {
     <div class="container py-5">
         <h2 class="fw-bold">{{ curApartment.title }}</h2>
         <p class="text-secondary fst-italic">A partire da <span class="text-success">{{ curApartment.price }}$</span> a notte</p>
-        <!-- Images -->
+
+        <div class="swiper-container">
+            <h3 class="mt-4">Foto appartamento ({{ curApartment.images.length }}) </h3>
+            <swiper
+                :modules="modules"
+                :slides-per-view="3"
+                :space-between="100"
+                :loop="true"
+                navigation
+                :pagination="{ clickable: true }"
+                :mousewheel="{ enabled: true }"
+                :autoplay="{delay: 3000}"
+                @swiper="onSwiper"
+                @slideChange="onSlideChange"
+            >
+        
+                <swiper-slide v-for="image in curApartment.images">
+                    <img :src="`${baseUrl}/storage/${image.image_path}`" alt="">
+                </swiper-slide>
+        
+            </swiper>
+
+        </div>
+
+        
+
+        <h3 class="mt-4">Dove trovarci</h3>
+
         <div v-if="!loading">
             <Map :latitude="lat" :longitude="lon" />
         </div>
@@ -43,4 +91,53 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+    .swiper-container{
+        width: 100%;
+        margin: 2.5rem auto;
+
+        .swiper{
+            display: flex;
+            justify-content: center;
+            // width: 1145px;
+            width: calc((315px * 3) + (100px * 2));
+
+            .swiper-wrapper{
+            width: 100%;
+
+                .swiper-slide:active{
+                    filter: sepia(1);
+                }
+            }
+        }
+
+
+        img{
+            width: 315px;
+            height: 350px;
+            border-radius: 25px;
+        }
+
+        
+    }
+
+    #popup-block{
+        background-color: white;
+        box-shadow: 0 0 10px 5px rgb(0, 0, 0, 0.5);
+        padding: 0 1rem;
+        width: 20rem;
+        height: 20rem;
+        z-index: 9999;
+        position: absolute;
+        top: 40vh;
+        right: 30vw;
+        
+        i{
+            position: relative;
+            top: 0.5rem;
+            left: 17rem;
+            color: red;
+            cursor: pointer;
+        }
+    }
 </style>
